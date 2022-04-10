4e3d0046c748539458259f954466763cede0a6a3
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-04-10 20:35:18 +0300

    Update clickingLogo.js

--- src/js/clickingLogo.js
+++ src/js/clickingLogo.js
@@ -2,7 +2,7 @@ const headerHome = document.querySelector('.search-block');
 const headerLibrary = document.querySelector('.header-buttons');
 const gallery = document.querySelector('.gallery');
 
-// headerLibrary.classList.toggle('visually-hidden');
+headerLibrary.classList.toggle('visually-hidden');
 
 const jsLibrary = document.querySelector('.header-home .js-library');
 const jsIcon = document.querySelector('.header-library .js-icon');
