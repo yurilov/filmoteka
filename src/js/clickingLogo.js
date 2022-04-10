b0f7612b68c39eb01cbbd427da1fa96105c6ec3e
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-04-10 14:47:02 +0300

    клик по логотипу и my library

--- src/js/clickingLogo.js
+++ src/js/clickingLogo.js
@@ -1,10 +1,19 @@
-const contactIcon = document.querySelector('.contact-icon');
+const headerHome = document.querySelector('.header-home');
 const headerLibrary = document.querySelector('.header-library');
+const gallery = document.querySelector('.gallery');
 
-contactIcon.addEventListener('click', handleSubmit);
+headerLibrary.classList.toggle('visually-hidden');
 
-function handleSubmit(e) {
+const jsLibrary = document.querySelector('.header-home .js-library');
+const jsIcon = document.querySelector('.header-library .js-icon');
+
+jsIcon.addEventListener('click', opensOrCloses);
+jsLibrary.addEventListener('click', opensOrCloses);
+
+function opensOrCloses(e) {
     e.preventDefault();
 
-    headerLibrary.innerHTML = '';
+    headerHome.classList.toggle('visually-hidden');
+    headerLibrary.classList.toggle('visually-hidden');
+    gallery.classList.toggle('visually-hidden');
 };
