198a352b751ecdd428ae047a2af489644286b0ff
Yurii Shchepilov <89077590+yurilov@users.noreply.github.com>
2022-04-10 12:41:27 +0300

    рефакторинг

--- src/index.js
+++ src/index.js
@@ -4,23 +4,19 @@ import { fetchSearchByKeyword } from './js/API/searchByKeyword';
 import { fetchTrending } from './js/API/searchByKeyword';
 import { renderCardMovie } from './js/render-card';
 import { refs } from './js/getRefs';
-import { creatigAnArrayOfGenres } from './js/creatingAnArrayOfGenres';
-
+import { creatingAnArrayOfGenres } from './js/creatingAnArrayOfGenres';
 
 const movie = fetchTrending().then(movie => {
   const objDataMovie = movie;
-  
+
   const results = objDataMovie.results;
   console.log(results);
 
   const movieCard = results.map(result => {
     let date = result.release_date.slice(0, 4);
-    const arrGenres = [];
-    creatigAnArrayOfGenres(result,arrGenres);
+
+    const arrGenres = creatingAnArrayOfGenres(result);
     return renderCardMovie(result, arrGenres, date);
-    
   });
   refs.containerMovies.append(...movieCard);
 });
-
-
