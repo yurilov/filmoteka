5c79e88f83ba0b4dc36217f1fbb5dfa9fd274584
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-04-12 11:26:10 +0300

    перенес в рефс + анимация между кнопками Home, My library, нажатие по логотипу.

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -1,4 +1,11 @@
 export const refs = {
   searchInputRef: document.querySelector('.input'),
   containerMovies: document.querySelector('.gallery'),
+
+
+  headerHome: document.querySelector('.header-home'),
+  headerLibrary: document.querySelector('.header-library'),
+  jsLibrary: document.querySelector('.header-home .js-library'),
+  jsHome: document.querySelector('.header-library .js-home'),
+  jsIcon: document.querySelector('.header-library .js-icon'),
 };
