bc7c0aa5a1d97ae548290c9203944a9e6f40abe3
AnnaKucherenko <annakucherenko31@gmail.com>
2022-04-14 10:45:35 +0300

    Маржин для моб версії

--- src/js/render-card.js
+++ src/js/render-card.js
@@ -3,7 +3,7 @@ export const renderCardMovie = (data) => {
 
   wrapper.innerHTML = `
     <a href="${data.id}" class="movie-card link">
-      <img class="movie-card_img" src="https://image.tmdb.org/t/p/original${data.poster_link}" alt="${data.title}" loading="lazy" />
+      <img class="movie-card_img" src="${data.poster_link}" alt="${data.title}" loading="lazy" />
       <div class="movie-card_info">
         <p class="movie_title">
             ${data.original_title}
