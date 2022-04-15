aa93edf9e65fb2b8d87ed0b24aa62433f173eef0
Konurbaeva <konurbaevamadina@gmail.com>
2022-04-15 14:28:46 +0200

    removed unused comments

--- src/js/addToQueued.js
+++ src/js/addToQueued.js
@@ -5,9 +5,9 @@ export function addToQueued(data) {
     queueModalBtn.addEventListener("click", addToQueuedLocalStorage);
     
     function addToQueuedLocalStorage(){
-        // Parse the serialized data back into an aray of objects
+     
         arrayOfQueued = JSON.parse(localStorage.getItem('movieQueued')) || [];
-        // Push the new data onto the array
+       
         arrayOfQueued.push(data);
         queueModalBtn.textContent = 'ADDED TO QUEUED';
 
