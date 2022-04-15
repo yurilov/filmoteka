82804dd6d4a339f79c6d83d6791d7b372400418f
Konurbaeva <konurbaevamadina@gmail.com>
2022-04-15 12:59:02 +0200

    checking logs

--- src/js/addToQueued.js
+++ src/js/addToQueued.js
@@ -3,20 +3,17 @@ const QUEUE_STORAGE = [];
 export function addToQueued(data) {
     
     const queueModalBtn = document.querySelector('#queueModalBtn');
-    console.log('queueModalBtn: ' + queueModalBtn);
-
     queueModalBtn.addEventListener("click", addToQueuedLocalStorage);
 
     function addToQueuedLocalStorage(){
     const queuedLocalStorage = localStorage.setItem("movieQueued", JSON.stringify(data))
     console.log('queuedLocalStorage: ', queuedLocalStorage);
-    console.log('arrayOfQueued: ' + arrayOfQueued);
-
+   
     let existingEntries = JSON.parse(localStorage.getItem("movieQueued"));
     if(existingEntries == null) existingEntries = [];
 
     console.log('EXISTING ENTRIES: ' + JSON.stringify(existingEntries));
-    // QUEUE_STORAGE.push(arrayOfQueued);
-    console.log(QUEUE_STORAGE.push(arrayOfQueued));
+     return console.log('PUSHED: ' + QUEUE_STORAGE.push(arrayOfQueued));
+    // return QUEUE_STORAGE.push(arrayOfQueued);
  }
 }
