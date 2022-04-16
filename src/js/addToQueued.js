110f28796240578b1c1b3bb07b61d5aab294c696
Konurbaeva <konurbaevamadina@gmail.com>
2022-04-17 00:55:46 +0200

    document.getElementById('queueModalBtn').id = 'addedToQueuedMovie';

--- src/js/addToQueued.js
+++ src/js/addToQueued.js
@@ -5,26 +5,37 @@ export function addToQueued(data) {
     queueModalBtn.addEventListener("click", addToQueuedLocalStorage);
     
     function addToQueuedLocalStorage(){
-     
         arrayOfQueued = JSON.parse(localStorage.getItem('movieQueued')) || [];
        
         arrayOfQueued.push(data);
         removeEventListener();
-        removedFromQueuedLocalStorage();
+     
         localStorage.setItem('movieQueued', JSON.stringify(arrayOfQueued));
 
         const { id: movieId} = data;
         console.log(`MOVIEID: ${movieId}`);
 
-    }
 
-    function removedFromQueuedLocalStorage() {
-        queueModalBtn.textContent = 'DELETE FROM QUEUED';
-        
-       
+        const isMovieInLocalData = arrayOfQueued.find(movie => movie.id === movieId);
+
+        if (isMovieInLocalData) {
+            queueModalBtn.textContent = 'Delete from queued';
+            queueModalBtn.addEventListener('click', onDeleteFromQueued);
+          } else {
+            queueModalBtn.textContent = 'ADD TO QUEUED';  
+            queueModalBtn.addEventListener('click', onDeleteFromQueued);
+          }
     }
 
     function removeEventListener(){
+        queueModalBtn.removeEventListener('click', addToQueuedLocalStorage);
+    }
+
+    function onDeleteFromQueued(){
+        console.log('ON DELETE FROM QUEUED')
+        queueModalBtn.textContent = 'DELETE FROM QUEUED';
+
+        document.getElementById('queueModalBtn').id = 'addedToQueuedMovie';
         queueModalBtn.removeEventListener('click', addToQueuedLocalStorage);
     }
 }
