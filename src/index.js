7d07c874db88040254a1b3dee8bdb787b856cbb8
Yurii Shchepilov <89077590+yurilov@users.noreply.github.com>
2022-04-20 18:14:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './js/clickingLogo';
 import './js/playerForTrailer';
 import './js/backToTop';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
-const currentPage = localStorage.getItem('currentPage';
+const currentPage = localStorage.getItem('currentPage');
 Loading.hourglass('Loading...', {
   svgColor: '#FF6B08',
 });
